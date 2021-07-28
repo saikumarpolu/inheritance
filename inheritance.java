276367261031086189b2a25d87b2d79bc5561bdd
saikumar polu <saikumarpolu1998@gmail.com>
2021-07-28 21:50:22 +0530

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,26 @@
+
+/*advantages
+ 1. inheritance like code re-usablity
+ 2. achieve polymerphism i,e method over riding achieve
+
+Disadvantage
+  two class tightly coupled*/
+
+
+class Animal //parent class
+{
+	void eat();
+	{
+		System.out.println("iam eating");
+	}
+}
+
+class dog extends Animal //child class
+{
+	public Static void Main(String[]args);
+	{
+		dog d=new dog();
+	   d.eat();
+	}
+}
+
